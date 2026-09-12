29c8981d80f37d7cdf019f2d45127efd7b5dbdf2
Alissonfersoa <batista.als@gmail.com>
2026-09-12 20:32:15 -0300

    feat: add open meteo api client

---
+++ src/api/test_request.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+from src.api.open_meteo_client import OpenMateoClient
+
+client = OpenMateoClient()
+
+data = client.get_current_weather(
+  latitude=-23.1896,
+  longitude=-45.8841,
+)
+
+pprint(data)
